c3d2ff8dafc09618e58cc2154f97c84a203910b9
ErikFMH <90428026+ErikFMH@users.noreply.github.com>
2022-11-26 22:30:57 -0600

    Add files via upload

---
+++ recursos/saludo.js
@@ -0,0 +1,16 @@
+function Saludo() {
+    var mensaje = confirm("¿Hola?");
+        if (mensaje) {
+            alert("¡Hola!");
+        }
+        else {
+            alert("Adios");
+        }
+    }
+    console.log("hola desde la consola")
+
+
+
+
+
+    
